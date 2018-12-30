db136ee067957680ed54c83f1e192c003a447360
Luis Iturrios <iturrios3063@gmail.com>
2018-12-29 19:51:32 -0700

    Ver: 1.0.2 agregar parametro para no revisar los certificados SSL del sat

--- cfdiclient/autenticacion.py
+++ cfdiclient/autenticacion.py
@@ -113,7 +113,7 @@ class Autenticacion():
             'SOAPAction': self.SOAP_ACTION
         }
 
-        response = requests.post(self.SOAP_URL, data=soapreq, headers=headers)
+        response = requests.post(self.SOAP_URL, data=soapreq, headers=headers, verify=False)
 
         if response.status_code != requests.codes['ok']:
             if not response.text.startswith('<s:Envelope'):
