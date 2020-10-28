459d06160d74f281e53309a068b616ec073339ed
Fatake <pace_lozano@hotmail.com>
2020-10-28 14:30:43 -0600

    Añadiendo Certificados Ejemplo

--- ejemplo_completo.py
+++ ejemplo_completo.py
@@ -1,17 +1,5 @@
-""" ejemplo_completo.py
-
-Luis Iturrios 
-25 jun. 2020 13:23
-para Américo
-
-Que tal Americo.
-
-Realice unas modificaciones en el programa lo que hace es iniciar la solicitud
-y luego tratar de verificarla cada 60 segundos hasta que te da un estatus 
-completado para despues descargar los paquetes zip, en caso de que la solicitud 
-se quede en estatus de error el programa se para. 
-
-Saludos
+""" 
+ejemplo_completo.py
 """
 import base64
 import datetime
@@ -21,13 +9,13 @@ import time
 from cfdiclient import (Autenticacion, DescargaMasiva, Fiel, SolicitaDescarga,
                         VerificaSolicitudDescarga)
 
-RFC = 'IUAL9406031K4'
-FIEL_CER = 'asd.cer'
-FIEL_KEY = 'df.key'
-FIEL_PAS = ''
+RFC = 'ESI920427886'
+FIEL_CER = '20001000000200000192.cer'
+FIEL_KEY = '20001000000200000192.key'
+FIEL_PAS = '12345678a'
 FECHA_INICIAL = datetime.date(2020, 1, 1)
 FECHA_FINAL = datetime.date(2020, 6, 24)
-PATH = 'Inputs/IUAL9406031K4/'
+PATH = 'certificados/'
 
 cer_der = open(os.path.join(PATH, FIEL_CER), 'rb').read()
 key_der = open(os.path.join(PATH, FIEL_KEY), 'rb').read()
