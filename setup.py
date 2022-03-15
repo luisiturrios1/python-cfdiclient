87aa7697ab4d9c2865311372ebf8d7631053ac0f
Luis Iturrios <luisiturrios1@gmail.com>
2022-03-15 00:48:13 -0700

    include_package_data

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type='text/markdown',
     url='https://github.com/luisiturrios1/python-cfdiclient',
     packages=setuptools.find_packages(),
+    include_package_data=True
     classifiers=[
         'Programming Language :: Python :: 2.7',
         "Programming Language :: Python :: 3.6",
@@ -22,7 +23,7 @@ setuptools.setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
     ],
-    install_requires = [
+    install_requires=[
         'lxml>=4.2.5',
         'requests>=2.21.0',
         'pycryptodome>=3.7.2',
